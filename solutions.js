87262c21f332bb9e554f9c3e59aa2ff39501dcbc
Dillon Hess <dhess268@gmail.com>
2022-04-15 23:59:43 -0400

    first commit add solutions

---
+++ solutions.js
@@ -0,0 +1,11 @@
+
+// Changes position of a string containing a first and last name
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ')
+  }
+
+
+// returns Yes when true and No when false
+  function boolToWord( bool ){
+    return bool? "Yes":"No"
+  }
